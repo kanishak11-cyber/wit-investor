24c053985c3e21340f6ea6655c08d9ae7c0906dc
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-07-11 20:43:05 +0530

    init

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 
-const Home: NextPage = () => {
+
+const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
